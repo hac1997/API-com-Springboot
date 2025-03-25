35d63e9ade0c55a04ab095a57cce39c54b0c8f10
herick <herick.c1997@aluno.ifsc.edu.br>
2025-03-25 00:09:57 -0300

    acrescentando os métodos onterPorId, DeletarPorId e Atualizar

--- src/main/java/com/github/hac1997/primeiroProjeto/controller/produtoController.java
+++ src/main/java/com/github/hac1997/primeiroProjeto/controller/produtoController.java
@@ -1,6 +1,10 @@
 package com.github.hac1997.primeiroProjeto.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,13 +30,20 @@ public class ProdutoController {
         produtoRepository.save(produto);
         
         } 
-
-    @GetMapping
-    public Produto obterPorId(@RequestBody String id){
-
+        
+    @GetMapping("/{id}")
+    public Produto obterPorId(@PathVariable("id") String id){
         return produtoRepository.findById(id).orElse(null);
-
-
     }
 
+    @DeleteMapping("/{id}")
+    public void DeletarPorId(@PathVariable("id") String id){
+        produtoRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void Atualizar(@PathVariable("id") String id, @RequestBody Produto produto){
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
 }
